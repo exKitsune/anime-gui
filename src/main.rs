3fb45fcdc35520014315820df2761b64e9c64dc7
Jacob Hughes <j@jacobhughes.me>
2020-10-29 18:08:19 -0400

    Made compile

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
 extern crate iced;
 use iced::{
-    button, keyboard, pane_grid, scrollable, Align, Button, Column, Container,
-    Element, HorizontalAlignment, Length, PaneGrid, Sandbox, Scrollable,
-    Settings, Text,
+    button, keyboard, pane_grid, scrollable, Align, Button, Column, Container, Element,
+    HorizontalAlignment, Length, PaneGrid, Sandbox, Scrollable, Settings, Text,
 };
 
 pub fn main() {
     AnimeGUI::run(Settings::default())
 }
 
-enum GUI_State {
+enum GUIState {
     Downloading,
-    Browsing
+    Browsing,
+}
+
+impl Default for GUIState {
+    fn default() -> Self {
+        Self::Browsing
+    }
 }
 
 #[derive(Default)]
+struct Content;
+
+impl Content {
+    fn new(_: usize) -> Self {
+        Self
+    }
+}
+
 struct AnimeGUI {
-    gui_state: GUI_State::Downloading,
-    panes: pane_grid::State<Content>
+    gui_state: GUIState,
+    panes: pane_grid::State<Content>,
+    increment_button: button::State,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -25,7 +39,8 @@ enum Message {
     SwitchToBrowse,
     SwitchToDownload,
     StartSearch,
-    Resized(pane_grid::ResizeEvent)
+    IncrementPressed,
+    Resized(pane_grid::ResizeEvent),
 }
 
 impl Sandbox for AnimeGUI {
@@ -34,8 +49,9 @@ impl Sandbox for AnimeGUI {
     fn new() -> Self {
         let (gui_panes, _) = pane_grid::State::new(Content::new(0));
         AnimeGUI {
-            gui_state: GUI_State::Downloading,
-            panes: gui_panes
+            gui_state: GUIState::Downloading,
+            panes: gui_panes,
+            increment_button: button::State::new(),
         }
     }
 
@@ -46,33 +62,35 @@ impl Sandbox for AnimeGUI {
     fn update(&mut self, message: Message) {
         match message {
             Message::SwitchToBrowse => {
-                self.gui_state = GUI_State::Browsing;
-            },
+                self.gui_state = GUIState::Browsing;
+            }
             Message::SwitchToDownload => {
-                self.gui_state = GUI_State::Downloading;
-            },
-            Message::StartSearch => {
-
-            },
+                self.gui_state = GUIState::Downloading;
+            }
+            Message::IncrementPressed => {
+                println!("BUTTON PRESSED BOOYAH");
+            }
+            Message::StartSearch => {}
+            Message::Resized(event) => {
+                println!("WINDOW RESIZED: {:?} {:?}", event.split, event.ratio);
+            }
         }
     }
 
     fn view(&mut self) -> Element<Message> {
         let content = match self.gui_state {
-            GUI_State::Downloading =>
-                Column::new()
-                    .padding(50)
-                    .align_items(Align::Center)
-                    .push(
-                        Button::new(&mut self.increment_button, Text::new("Switch to Browsing"))
-                            .on_press(Message::IncrementPressed),
-                    )
-                    .push(Text::new("Downloading").size(20))
-                    .into()
-            GUI_State::Browsing =>
-                Column::new()
-                    .spacing(20)
-                    .push(Text::new("Browsing").size(20))
+            GUIState::Downloading => Column::new()
+                .padding(50)
+                .align_items(Align::Center)
+                .push(
+                    Button::new(&mut self.increment_button, Text::new("Switch to Browsing"))
+                        .on_press(Message::IncrementPressed),
+                )
+                .push(Text::new("Downloading").size(20))
+                .into(),
+            GUIState::Browsing => Column::new()
+                .spacing(20)
+                .push(Text::new("Browsing").size(20)),
         };
 
         Container::new(content)
